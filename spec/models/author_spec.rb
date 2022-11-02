22287c8869907658af880f2d2430d44c726505fa
antonneubauer <antongneubauer@gmail.com>
2022-11-02 14:39:46 +0100

    failing test for #3

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,11 @@
+describe Author, type: :model do
+    it "should create new instance of author" do
+        first_name = 'Alan'
+        last_name = 'Turing'
+        homepage = 'http://wikipedia.org/Alan_Turing'
+        author = Author.new(:first_name => first_name, :last_name => last_name, :homepage => homepage)
+        expect(author.first_name).to eq(first_name)
+        expect(author.last_name).to eq(last_name)
+        expect(author.homepage).to eq(homepage)
+    end
+end
