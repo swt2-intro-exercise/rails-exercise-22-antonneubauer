1f9e22c36e12ee0949f9031be242984c49c736fb
antonneubauer <antongneubauer@gmail.com>
2022-11-02 14:12:10 +0100

    maybe now its fixed... #2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rails routes".
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'home/index'
+
   resources :authors
 
   # You can have the root of your site routed with "root"
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,11 +5,11 @@ describe "New author page", type: :feature do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
   end
-  
+
   it "should check for input fields for first_name, last_name and homepage at 'new_author_path'" do
     visit new_author_path
-    expect(page).to have_field('authors[first_name]')
-    expect(page).to have_field('authors[last_name]')
-    expect(page).to have_field('authors[homepage]')
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
   end
 end
