d827d5c9f109e2cb6034e7fde4a504174b708171
antonneubauer <antongneubauer@gmail.com>
2022-11-13 16:30:07 +0100

    failing test for #7

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :author do
-    first_name { "Alan" }
-    last_name { "Turing" }
-    homepage { "http://wikipedia.de/Alan_Turing" }
+    first_name { 'Alan' }
+    last_name { 'Turing' }
+    homepage { 'http://wikipedia.de/Alan_Turing' }
+  end
+
+  factory :invalide_author do
+    first_name { 'Alan' }
+    last_name { '' }
+    homepage { 'http://wikipedia.de/Alan_Turing' }
   end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,19 +1,24 @@
 describe Author, type: :model do
-    it "should create new instance of author" do
-        first_name = 'Alan'
-        last_name = 'Turing'
-        homepage = 'http://wikipedia.org/Alan_Turing'
-        author = Author.new(:first_name => first_name, :last_name => last_name, :homepage => homepage)
-        expect(author.first_name).to eq(first_name)
-        expect(author.last_name).to eq(last_name)
-        expect(author.homepage).to eq(homepage)
-    end
+  it 'should create new instance of author' do
+    first_name = 'Alan'
+    last_name = 'Turing'
+    homepage = 'http://wikipedia.org/Alan_Turing'
+    author = Author.new(first_name: first_name, last_name: last_name, homepage: homepage)
+    expect(author.first_name).to eq(first_name)
+    expect(author.last_name).to eq(last_name)
+    expect(author.homepage).to eq(homepage)
+  end
 
-    it ".name should return the full name" do
-        first_name = 'Alan'
-        last_name = 'Turing'
-        homepage = ''
-        author = Author.new(:first_name => first_name, :last_name => last_name, :homepage => homepage)
-        expect(author.name).to eq(first_name + " " + last_name)
-    end
+  it '.name should return the full name' do
+    first_name = 'Alan'
+    last_name = 'Turing'
+    homepage = ''
+    author = Author.new(first_name: first_name, last_name: last_name, homepage: homepage)
+    expect(author.name).to eq(first_name + ' ' + last_name)
+  end
+
+  it 'should not validate with a lastname shorter than 1 char' do
+    @invalid_alan = FactoryBot.create :invalide_author
+    expect(@invalid_alan).to_not be_valid
+  end
 end
