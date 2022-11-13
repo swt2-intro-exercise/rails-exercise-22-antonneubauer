3586c7b8a2ddf41b74c2b22ada2fc83fbcd32c09
antonneubauer <antongneubauer@gmail.com>
2022-11-13 18:15:25 +0100

    failing test #8

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "New author page", type: :feature do
+describe 'New author page', type: :feature do
   it "should exist at 'new_author_path' and render withour error" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
@@ -11,5 +11,13 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
+  end
+
+  it 'should display an error when an invalid author is supposed to be created' do
+    visit new_author_path
+    fill_in 'author[first_name]', with: 'Alan'
+    fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+    click_button 'Save Author'
+    expect(page).to have_text("Last name can't be blank")
   end
 end
