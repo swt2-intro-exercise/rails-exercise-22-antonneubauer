d881b5894b6d2adb39c20f2de77d720096c08ca1
antonneubauer <antongneubauer@gmail.com>
2022-11-16 21:00:40 +0100

    fix #13

--- spec/views/papers/index.html.erb_spec.rb
+++ spec/views/papers/index.html.erb_spec.rb
@@ -1,26 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe "papers/index", type: :view do
-  before(:each) do
-    assign(:papers, [
-      Paper.create!(
-        title: "Title",
-        venue: "Venue",
-        year: 2
-      ),
-      Paper.create!(
-        title: "Title",
-        venue: "Venue",
-        year: 2
-      )
-    ])
+RSpec.describe "papers/index", type: :feature do
+  # before(:each) do
+  #   assign(:papers, [
+  #     Paper.create!(
+  #       title: "Title",
+  #       venue: "Venue",
+  #       year: 2
+  #     ),
+  #     Paper.create!(
+  #       title: "Title",
+  #       venue: "Venue",
+  #       year: 2
+  #     )
+  #   ])
+  # end
+
+  it "should exist at 'papers_path' and render without error" do
+    paper_one = FactoryBot.create :paper
+    paper_two = FactoryBot.create :paper
+    visit papers_path
+
+    expect(page).to have_selector('table tr th', text: 'Title')
+    expect(page).to have_selector('table tr th', text: 'Venue')
+    expect(page).to have_selector('table tr th', text: 'Year')
+
+    expect(page).to have_text(paper_one.title)
+    expect(page).to have_text(paper_one.venue)
+    expect(page).to have_text(paper_one.year)
+    expect(page).to have_link('Show', href: paper_path(paper_one))
+    expect(page).to have_link('Edit', href: edit_paper_path(paper_one))
+    expect(page).to have_link('Delete', href: paper_path(paper_one))
+    expect(page).to have_text(paper_two.title)
+    expect(page).to have_text(paper_two.venue)
+    expect(page).to have_text(paper_one.year)
+    expect(page).to have_link('Show', href: paper_path(paper_two))
+    expect(page).to have_link('Edit', href: edit_paper_path(paper_two))
+    expect(page).to have_link('Delete', href: paper_path(paper_two))
+
+    expect(page).to have_link('New', href: new_paper_path)
   end
 
-  it "renders a list of papers" do
-    render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Venue".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
+  it "renders a link to the show, edit, delete page of a paper" do
+    a_paper = FactoryBot.create :paper
+    visit papers_path
+    
+    expect(page).to have_link('Show', href: paper_path(a_paper))
+    expect(page).to have_link('Edit', href: edit_paper_path(a_paper))
+    expect(page).to have_link('Delete', href: paper_path(a_paper))
   end
 end
