56b6ab78df87142659e2daa01513f17683e11ed4
antonneubauer <antongneubauer@gmail.com>
2022-11-16 20:39:41 +0100

    failing test #12

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -14,4 +14,34 @@ describe Paper, type: :model do
     expect(a_paper.venue).to eq(venue)
     expect(a_paper.year).to eq(year)
   end
+
+  it 'should not validate without a title, year has to be a numeric value' do
+    title = 'Title1'
+    venue = 'venue1'
+    year = 1900
+    invalid_paper_one = Paper.new(
+      title: title,
+      venue: venue
+    )
+    expect(invalid_paper_one).to_not be_valid
+
+    invalid_paper_two = Paper.new(
+      title: title,
+      year: year
+    )
+    expect(invalid_paper_one).to_not be_valid
+
+    invalid_paper_three = Paper.new(
+      venue: venue,
+      year: year
+    )
+    expect(invalid_paper_three).to_not be_valid
+
+    invalid_paper_four = Paper.new(
+      title: title,
+      venue: venue,
+      year: venue
+    )
+    expect(invalid_paper_three).to_not be_valid
+  end
 end
